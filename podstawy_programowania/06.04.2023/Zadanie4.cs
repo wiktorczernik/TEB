da4162333535ae70b2a5e950c14e53fd404a1985
Nickname <88354270+Nickn4meDEV@users.noreply.github.com>
2023-04-06 14:38:04 +0200

    Zadanie 4

---
+++ podstawy_programowania/06.04.2023/Zadanie4.cs
@@ -0,0 +1,21 @@
+const int m = 7;
+int[] y = new int[m];
+
+Console.WriteLine("Tablica Indexowana:\n");
+
+y[0] = -1;
+Console.Write(y[0]);
+
+for (int i = 1; i < m; i++)
+{
+    y[i] = i;
+    if (y[i] % 2 == 0)
+    {
+        y[i] = i * i;
+    }
+    else
+    {
+        y[i] = i - 1;
+    }
+    Console.Write(", " + y[i]);
+}
